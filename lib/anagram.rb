36e5d1690bdf0c05dba79108739ece36b224b509
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-15 18:48:22 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,6 +7,6 @@ class Anagram
   end
   
   def match(array_of_items)
-    array_of_items.filter {|item| item.split(''.)sort == @word.split('').sort}
+    array_of_items.filter {|item| item.split('').sort == @word.split('').sort}
   end
 end
